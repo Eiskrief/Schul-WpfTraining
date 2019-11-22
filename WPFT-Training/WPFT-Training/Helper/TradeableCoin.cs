bfad09191ad547568cb1ff4d473f330c12e16634
Johannes Kanes <j.kanes@traveltrex.com>
2019-11-22 13:09:47 +0100

    Minor fixes in Order to make App more readable

---
+++ WPFT-Training/WPFT-Training/Helper/TradeableCoin.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace WPFT_Training.Helper
+{
+    public class TradeableCoin
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+        [JsonProperty(PropertyName = "rank")]
+        public string Rank { get; set; }
+        [JsonProperty(PropertyName = "price_usd")]
+        public string PriceUsd { get; set; }
+        [JsonProperty(PropertyName = "24h_volume_usd")]   
+        public string VolumeUsd24H { get; set; }
+        [JsonProperty(PropertyName = "market_cap_usd")]
+        public string MarketCapUsd { get; set; }
+        [JsonProperty(PropertyName = "available_supply")]
+        public string AvailableSupply { get; set; }
+        [JsonProperty(PropertyName = "total_supply")]
+        public string TotalSupply { get; set; }
+        [JsonProperty(PropertyName = "percent_change_1h")]
+        public string PercentChange1H { get; set; }
+        [JsonProperty(PropertyName = "percent_change_24h")]
+        public string PercentChange24H { get; set; }
+        [JsonProperty(PropertyName = "percent_change_7d")]
+        public string PercentChange7D { get; set; }
+        [JsonProperty(PropertyName = "last_updated")]
+        public string LastUpdated { get; set; }
+    }
+}
--- WPFT-Training/WPFT-Training/MainWindow.xaml.cs
+++ WPFT-Training/WPFT-Training/MainWindow.xaml.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.IO;
+using System.Net.Http;
 using Newtonsoft.Json;
-using GetBookingValues.Models;
-using GetBookingValues.Controllers;
-using System.Web;
-
-
-
+using WPFT_Training.Helper;
 
 namespace WPFT_Training
 {
@@ -34,18 +16,18 @@ namespace WPFT_Training
         public MainWindow()
         {
             InitializeComponent();
-
-           
-            
-                
-            
         }
 
-        private void loadButton_Click(object sender, RoutedEventArgs e)
+        private async void loadButton_Click(object sender, RoutedEventArgs e)
         {
-            List<ListElements> test = ListElementsController.GetListElements();
 
-            dataGrid1.ItemsSource = test;
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://api.coinmarketcap.com/v1/ticker/");
+            var content = await response.Content.ReadAsStringAsync();
+            var tradeableCoinsList = JsonConvert.DeserializeObject<List<TradeableCoin>>(content);
+
+            this.TradeableCoinsDataGrid.ItemsSource = tradeableCoinsList;
+
         }
     }
     
